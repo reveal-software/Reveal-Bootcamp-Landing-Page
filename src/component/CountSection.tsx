0836a2a33acd298df4916cef465e5ee3311acd13
LeulAria <leulsegedtariku@gmail.com>
2022-12-06 12:19:57 +0300

    add sample assets on bottom

--- src/component/CountSection.tsx
+++ src/component/CountSection.tsx
@@ -1,29 +1,41 @@
 import React from "react";
 import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 const CountSection = () => {
+  const { ref, inView, entry } = useInView({
+    threshold: 0
+  });
+
   return (
-    <div className="max-w-[1050px] w-full mt-[150px] mx-auto grid grid-cols-3 gap-6">
-      <div className="text-center">
-        <h1 className="text-5xl mb-2">
-          <CountUp end={7} duration={1} />
-        </h1>
-        <h1 className="text-xl">Students Enrolled</h1>
-      </div>
+    <div
+      ref={ref}
+      className="max-w-[1050px] w-full mt-[150px] mx-auto grid grid-cols-3 gap-6"
+    >
+      {inView && (
+        <>
+          <div className="text-center">
+            <h1 className="text-5xl mb-2">
+              <CountUp end={7} duration={1} />
+            </h1>
+            <h1 className="text-xl">Students Enrolled</h1>
+          </div>
 
-      <div className="text-center">
-        <h1 className="text-5xl mb-2">
-          <CountUp end={7} duration={5} />
-        </h1>
-        <h1 className="text-xl">Instructors</h1>
-      </div>
+          <div className="text-center">
+            <h1 className="text-5xl mb-2">
+              <CountUp end={5} duration={2} />
+            </h1>
+            <h1 className="text-xl">Instructors</h1>
+          </div>
 
-      <div className="text-center">
-        <h1 className="text-5xl mb-2">
-          <CountUp end={25} duration={4} />
-        </h1>
-        <h1 className="text-xl">Real World Projects</h1>
-      </div>
+          <div className="text-center">
+            <h1 className="text-5xl mb-2">
+              <CountUp end={25} duration={4} />
+            </h1>
+            <h1 className="text-xl">Real World Projects</h1>
+          </div>
+        </>
+      )}
     </div>
   );
 };
